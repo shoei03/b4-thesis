3e4ebfe96b389d7cf30728131328b4ce092e4e25
shoei03 <shoei.yoshida.03@gmail.com>
2026-01-18 14:51:27 +0900

    union-findクラスを作成

---
+++ src/b4_thesis/core/track/union_find.py
@@ -0,0 +1,16 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self.parent[root_y] = root_x
